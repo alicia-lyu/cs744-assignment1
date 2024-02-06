2f9725dfc303c80c1ee0fb735b9387cae6bef472
alicia-lyu <wlyu27@wisc.edu>
2024-02-06 17:20:53 -0600

    DRY

---
+++ part3/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# $1: task_num
+# $2: data file/dir name, either web-BerkStan.txt or enwiki-pages-articles
+
+# Make sure /part3 exists
+if ! hadoop fs -test -d /part3 ; then
+    hadoop fs -mkdir /part3
+    echo "hadoop fs mkdir part3"
+fi
+
+# Make sure /part3/datadir exists
+
+if [ $2 == "web-BerkStan.txt" ]; then
+    $dir_by_data="/part3/web"
+elif [ $2 == "enwiki-pages-articles" ]; then
+    $dir_by_data="/part3/wiki"
+else
+    echo "Invalid data"
+    exit 1
+fi
+
+if ! hadoop fs -test -d $dir_by_data ; then
+    hadoop fs -mkdir $dir_by_data
+    echo "hadoop fs mkdir $dir_by_data"
+fi
+
+# Make sure /part3/datadir/task$1 exists
+if ! hadoop fs -test -d $dir_by_data/task$1 ; then
+    hadoop fs -mkdir $dir_by_data/task$1
+    echo "hadoop fs mkdir task$1"
+fi
+
+# Make sure data file is in hdfs
+if ! hadoop fs -test -e /part3/task1/web-BerkStan.txt ; then
+	hadoop fs -copyFromLocal /mnt/data/datasets/web-BerkStan.txt /data/
+	echo "hadoop fs copyFromLocal"
+fi
+
+# spark-submit
+/mnt/data/spark-3.3.4-bin-hadoop3/bin/spark-submit \
+  main.py \
+  "hdfs://10.10.1.1:9000/data/$2" "hdfs://10.10.1.1:9000/$dir_by_data/task$1/output" 
