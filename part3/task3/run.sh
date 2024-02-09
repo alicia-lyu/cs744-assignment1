8ef2b3440f847cd7c15b97b1e28c8b7dc03a534b
alicia-lyu <wlyu27@wisc.edu>
2024-02-08 21:24:58 -0600

    run.sh

--- part3/task3/run.sh
+++ part3/task3/run.sh
@@ -1,4 +1,4 @@
 echo "Running task 3 with web-BerkStan.txt"
-../run_small 3 web-BerkStan.txt || echo "Failed to run task 3 with web-BerkStan.txt"
+../run_small.sh 3 web-BerkStan.txt || echo "Failed to run task 3 with web-BerkStan.txt"
 echo "Running task 1 with enwiki-pages-articles"
-../run_big 3 1 || echo "Failed to run task 3 with enwiki-pages-articles"
+../run_big.sh 3 1 || echo "Failed to run task 3 with enwiki-pages-articles"
--- part3/task4/run.sh
+++ part3/task4/run.sh
@@ -1,4 +1,4 @@
 echo "Running task 4 with web-BerkStan.txt, but you have to manually remove a worker"
-../run_small 4 web-BerkStan.txt || echo "Failed to run task 4 with web-BerkStan.txt"
+../run_small.sh 4 web-BerkStan.txt || echo "Failed to run task 4 with web-BerkStan.txt"
 echo "Running task 4 with enwiki-pages-articles, but you have to manually remove a worker"
-../run_big 4 1 || echo "Failed to run task 4 with enwiki-pages-articles"
+../run_big.sh 4 1 || echo "Failed to run task 4 with enwiki-pages-articles"
